ec38387aaf6eebd010ba51489595dff037d1bb07
kipkoechke <chepkwonyke1@gmail.com>
2024-05-07 14:07:31 +0300

    Chore: Added emojis

--- travel-list/src/components/Stats.js
+++ travel-list/src/components/Stats.js
@@ -2,7 +2,7 @@ export default function Stats({ items }) {
   if (!items.length)
     return (
       <footer className="stats">
-        <em>Start adding some items to your packing list.</em>
+        <em>Start adding some items to your packing list. 😀</em>
       </footer>
     );
   const numItems = items.length;
@@ -12,10 +12,11 @@ export default function Stats({ items }) {
     <footer className="stats">
       <em>
         {percentage === 100
-          ? "You got everything! Ready to go"
-          : ` You have ${numItems} items on your list, and you already packed ${numPacked} 
+          ? "You got everything! Ready to go ✈️"
+          : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} 
        (${percentage}%)`}
       </em>
     </footer>
   );
 }
+ 
