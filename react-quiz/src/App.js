ffe8d307fdcaffb90079de39d01794609de7ed99
kipkoechke <chepkwonyke1@gmail.com>
2024-05-09 19:39:55 +0300

    Chore: Added actions for the question

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -25,6 +26,11 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
     default:
       throw new Error("Unkown action");
   }
@@ -45,8 +51,11 @@ function App() {
       <Header />
       <Main>
         {status === "loading" && <Loader />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatcher} />
+        )}
         {status === "error" && <Error />}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
