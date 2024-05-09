b768fbfac3efe9e3cd07ccc5c5273e432215e662
kipkoechke <chepkwonyke1@gmail.com>
2024-05-09 21:29:02 +0300

    Chore: Worked on displaying progress

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -6,6 +6,7 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -62,6 +63,10 @@ function App() {
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, curr) => prev + curr.points,
+    0,
+  );
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -80,6 +85,13 @@ function App() {
         {status === "error" && <Error />}
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
