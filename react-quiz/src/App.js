aedb7e201885eaef8572fe9b70e0d7b984a57cd2
kipkoechke <chepkwonyke1@gmail.com>
2024-05-09 11:22:25 +0300

    Chore: Implemented logic to fetch data from an api

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,22 @@
-import DateCounter from "./DateCounter";
-
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <h1>React Quiz</h1>
+        <p>
+          This is a simple React app that will test your knowledge of React.
+        </p>
+      </Main>
     </div>
   );
 }
