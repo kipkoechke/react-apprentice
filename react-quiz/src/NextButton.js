1b52cba3ae46122eabdb188a60c864bafa8544f1
kipkoechke <chepkwonyke1@gmail.com>
2024-05-09 22:35:35 +0300

    Chore: Implemented quiz finish

--- react-quiz/src/NextButton.js
+++ react-quiz/src/NextButton.js
@@ -1,16 +1,30 @@
-function NextButton({ answer, dispatch }) {
-  return (
-    <div>
-      {answer !== null && (
+function NextButton({ answer, dispatch, index, numQuestions }) {
+  if (answer === null) return null;
+  if (index < numQuestions - 1)
+    return (
+      <div>
         <button
           className="btn btn-ui"
           onClick={() => dispatch({ type: "nextQuestion" })}
         >
           Next
         </button>
-      )}
-    </div>
-  );
+      </div>
+    );
+
+  if (index === numQuestions - 1)
+    return (
+      <div>
+        (
+        <button
+          className="btn btn-ui"
+          onClick={() => dispatch({ type: "finish" })}
+        >
+          Finish
+        </button>
+        )
+      </div>
+    );
 }
 
 export default NextButton;
