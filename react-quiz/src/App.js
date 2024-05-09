4600a7efec1bbdfd17cc30afde6b49e6e42be964
kipkoechke <chepkwonyke1@gmail.com>
2024-05-09 11:36:47 +0300

    Chore: Loaded data from the fake api

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,12 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+
+const initialState = {
+  questions: [],
+
+  // "loading","error", "ready", "active", "finished"
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        status: "ready",
+        questions: action.payload,
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Unkown action");
+  }
+}
 function App() {
+  const [state, dispatcher] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
+      .then((data) => dispatcher({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatcher({ type: "dataFailed", payload: err }));
   }, []);
   return (
     <div className="app">
