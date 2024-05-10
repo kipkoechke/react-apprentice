043c55d3941a9189858c568e1a8662a44022f8eb
kipkoechke <chepkwonyke1@gmail.com>
2024-05-10 23:42:00 +0300

    Chore: Implemented countries list

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -7,6 +7,7 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "../components/CityList";
 import { useEffect, useState } from "react";
+import CountryList from "../components/CountryList";
 
 const BASE_URL = "http://localhost:8000";
 function App() {
@@ -45,7 +46,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
