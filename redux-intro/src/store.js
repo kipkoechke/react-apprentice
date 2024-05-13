f34cf25e5b8a557e4096a867c621758f5436e527
kipkoechke <chepkwonyke1@gmail.com>
2024-05-13 18:39:47 +0300

    Chore: Added more customer state

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,11 +1,12 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
-function reducer(state = initialState, action) {
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return {
@@ -38,7 +39,56 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName: fullName,
+      nationalID: nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  };
+}
+
+function updateName(fullName) {
+  return {
+    type: "customer/updateName",
+    payload: fullName,
+  };
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
 
 function deposit(amount) {
   return {
@@ -71,4 +121,8 @@ console.log(store.getState());
 store.dispatch(requestLoan(1000, "Buy a car"));
 console.log(store.getState());
 store.dispatch(payLoan());
+console.log(store.getState());
+store.dispatch(createCustomer("Kelvin Chepkwony", "1234567890"));
+console.log(store.getState());
+store.dispatch(updateName("K Delvinoe"));
 console.log(store.getState());
