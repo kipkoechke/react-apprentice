1190a6738f8ddcc0ddef03a8e24ebbac8736ca37
kipkoechke <chepkwonyke1@gmail.com>
2024-05-13 17:44:56 +0300

    Chore: Created redux store

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -19,17 +21,35 @@ function reducer(state = initialState, action) {
       if (state.loan > 0) return state;
       return {
         ...state,
-        loan: action.payload,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
       };
     case "account/payLoan":
       return {
         ...state,
         loan: 0,
         loanPurpose: "",
-        balance: state.balance - action.payload,
+        balance: state.balance - state.loan,
       };
 
     default:
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+
+console.log(store.getState());
+store.dispatch({ type: "account/withdraw", payload: 200 });
+
+console.log(store.getState());
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Buy a car" },
+});
+console.log(store.getState());
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
