abaa2ee411dd4f61a145d3c9ce4b861843123a59
kipkoechke <chepkwonyke1@gmail.com>
2024-05-18 18:53:20 +0300

    Chore: Optimized fonts

--- the-wild-oasis/app/layout.js
+++ the-wild-oasis/app/layout.js
@@ -2,6 +2,10 @@ import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
+
 export const metadata = {
   title: "The Wild Oasis",
   description:
@@ -11,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <Logo />
         <Navigation />
         <main>{children}</main>
