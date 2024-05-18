eb368e734d730424c85649c2605f342832cfc473
kipkoechke <chepkwonyke1@gmail.com>
2024-05-18 20:15:25 +0300

    Chore: Modified the account page

--- the-wild-oasis/app/account/page.js
+++ the-wild-oasis/app/account/page.js
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1>Account Page!</h1>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Bellon
+    </h2>
+  );
 }
