1c1fff4e590a04ea2e82fda794c90cd0d73b5b61
Kelvin Kipkoech Chepkwony <chepkwonyke1@GMAIL.COM>
2025-02-21 16:53:17 +0300

    Fix: update dependencies

--- eventz/src/components/ui/calendar.tsx
+++ eventz/src/components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
@@ -59,13 +59,21 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
+      // components={{
+      //   IconLeft: ({ className, ...props }) => (
+      //     <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+      //   ),
+      //   IconRight: ({ className, ...props }) => (
+      //     <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+      //   ),
+      // }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Chevron: (props) => {
+          if (props.orientation === "left") {
+            return <ChevronLeftIcon {...props} />;
+          }
+          return <ChevronRightIcon {...props} />;
+        },
       }}
       {...props}
     />
