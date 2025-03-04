89b2b4aa3b6c5cc1ef1c875be65a5cea14de42fa
Kelvin Kipkoech Chepkwony <chepkwonyke1@GMAIL.COM>
2025-03-04 17:55:26 +0300

    Chore: add supabase configs

---
+++ 24-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+export const supabaseUrl = "https://feupzfoeulkrimngctol.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
