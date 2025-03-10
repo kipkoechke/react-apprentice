a4bdf09cae3c80950738d5ed5c15aac88944cb21
kipkoechke <chepkwonyke1@gmail.com>
2025-03-10 12:12:23 +0300

    Init: project setup

---
+++ portfolio/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const jetBrainsMono = JetBrains_Mono({
+  variable: "--font-jetBrainsMono",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Kelvin portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${jetBrainsMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ portfolio/tailwind.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetBrainsMono)",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetBrainsMono)",
+    },
+    extend: {
+      colors: {
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        },
+        grey: "737373",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+  purge: ["./src/**/*.{js,jsx,ts,tsx,mdx}", "./public/index.html"],
+} satisfies Config;
